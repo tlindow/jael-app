3cfd96f601b52bc535e07dd38c8e55734f0450ce
Tyler Lindow <tyler.lindow@gmail.com>
2019-12-08 23:27:10 -0800

    Initialize app

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.static('public'));
+
+app.listen(port, () => 
+  console.log(`Example app listening on port ${port}!`)
+)
